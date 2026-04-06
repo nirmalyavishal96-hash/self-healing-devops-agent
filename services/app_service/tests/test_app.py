c5d0c63f696f5ecb8a32cd264e7099fea4541eef
Nirmalya Das <nirmalyavishal96@gmail.com>
2026-04-06 17:13:55 +0000

    pipeline updated

--- services/app_service/tests/test_app.py
+++ services/app_service/tests/test_app.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+
 
 def test_metrics():
     client = app.test_client()
